74c8cb99240b7b021704b21c461a128e71f33eb7
Yoshida24 <trihead1.dev@gmail.com>
2023-11-27 00:56:21 +0900

    feat: reboot

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -2,4 +2,4 @@
 python3 -m venv .venv && . .venv/bin/activate
 # git pull origin main
 # pip install -r requirements.txt
-echo "System will reboot in 30 seconds..." & (sleep 30s && sudo shutdown now -r) &
+echo "System will reboot in 30 seconds..." & (sleep 30s && sudo reboot) &
